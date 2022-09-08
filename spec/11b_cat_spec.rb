a5a5ccfc9a313a3420995966f347f9af05a5a5a1
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-08 09:23:05 -0500

    complete shared examples exercise

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
+  subject(:mr_floof) { described_class.new('Mr. Floof', 'Siamese', 'Grey') }
 
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat is a child class of Pet' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(mr_floof).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(mr_floof).to be_hiding
   end
 end
