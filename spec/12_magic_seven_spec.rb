c5a836767b7c4fdc11e66d8c6ede7f918a6f212c
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-08 09:41:26 -0500

    complete exercise 12

--- spec/12_magic_seven_spec.rb
+++ spec/12_magic_seven_spec.rb
@@ -70,34 +70,78 @@ describe MagicSeven do
   # Write a test for each of the following methods:
 
   describe '#subtract_four' do
+    context 'when the previous step is 5' do
+      it 'returns 1' do
+        random_number = 5
+
+        result = game.subtract_four(random_number)
+        expect(result).to be(1)
+      end
+    end
   end
 
   describe '#divide_by_two' do
+    context 'when the previous step is 8' do
+      it 'returns 4' do
+        random_number = 8
+
+        result = game.divide_by_two(random_number)
+        expect(result).to be(4)
+      end
+    end
+
+    context 'when the previous step is 9' do
+      it 'returns 4' do
+        random_number = 9
+
+        result = game.divide_by_two(random_number)
+        expect(result).to be(4)
+      end
+    end
   end
 
   # The following tests will need you to create new instances of MagicSeven with
   # a specific value for the random_number.
   describe '#subtract_random_number' do
+    let(:six) { described_class.new(6) }
+
+    context 'when MagicSeven has value 6 and previous step is 9' do
+      it 'returns 3' do
+        random_number = 9
+
+        result = six.subtract_random_number(random_number)
+        expect(result).to be(3)
+      end
+    end
   end
 
   # The #play method will always return seven! Test this game, using any
   # integer as the random_number. Update the context with the number.
   describe '#play' do
-    context 'when the random number is ...' do
-      # remove the 'x' before running this test
-      xit 'will return 7' do
+    context 'when the random number is 10' do
+      let(:magic) { described_class.new(10) }
+
+      it 'will return 7' do
+        result = magic.play
+        expect(result).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
-      # remove the 'x' before running this test
-      xit 'will return 7' do
+    context 'when the random number is -6' do
+      let(:magic) { described_class.new(-6) }
+
+      it 'will return 7' do
+        result = magic.play
+        expect(result).to eq(7)
       end
     end
 
-    context 'when the random number is ...' do
-      # remove the 'x' before running this test
-      xit 'will return 7' do
+    context 'when the random number is 5,678' do
+      let(:magic) { described_class.new(5_768) }
+
+      it 'will return 7' do
+        result = magic.play
+        expect(result).to eq(7)
       end
     end
   end
